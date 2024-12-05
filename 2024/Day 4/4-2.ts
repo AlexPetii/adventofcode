e62cead0155ef186694289e78151a5410b3da73d
Peti <alexandrpet195@gmail.com>
2024-12-05 21:51:26 +0000

    Day 4 Part 2, typescript

---
+++ 2024/Day 4/4-2.ts
@@ -0,0 +1,38 @@
+import * as fs from "node:fs";
+
+function countXMAS(grid: string[][]): number {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let count = 0;
+
+  const isMas = (str: string): boolean => str === "MAS" || str === "SAM";
+
+  for (let x = 1; x < rows - 1; x++) {
+    for (let y = 1; y < cols - 1; y++) {
+      const diag1 = grid[x - 1][y - 1] + grid[x][y] + grid[x + 1][y + 1];
+
+      const diag2 = grid[x - 1][y + 1] + grid[x][y] + grid[x + 1][y - 1];
+
+      if (isMas(diag1) && isMas(diag2)) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error readFile ", err);
+    return;
+  }
+
+  const grid: string[][] = data
+    .trim()
+    .split("\n")
+    .map((line) => line.trim().split(""));
+
+  const result = countXMAS(grid);
+
+  console.log("Total X-MAS: ", result);
+});
